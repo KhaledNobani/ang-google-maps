677a15525bed5191f12788289b6f2e7825d56e04
Khaled <khaled.z.nobani@gmail.com>
2015-08-04 11:09:24 +0700

    feat(autocomplete-for-places): adding autocomplete directive

--- src/module.js
+++ src/module.js
@@ -2,8 +2,15 @@
 
     'use strict';
     ang.module('angular-google-maps', [])
-        .directive('mapTemp', ['$window', '$document', '$http', mapTempFactory]);
+        .directive('mapTemp', ['$window', '$document', '$http', mapTempFactory])
+        .directive('autoCompleteTemp', ['$window', '$http', autoCompleteTempFactory]);
 
+    /**
+      * Creates the factory of mapTemp.
+      *
+      * @param {Object} $window
+      * @param {Object} $http
+      */
     function mapTempFactory($window, $document, $http) {
 
         return {
@@ -22,6 +29,27 @@
     }
 
     /**
+      * Creates the factory of autoCompleteTemp.
+      *
+      * @param {Object} $window
+      * @param {Object} $http
+      */
+    function autoCompleteTempFactory($window, $http) {
+
+        return {
+
+            controller: autoCompleteTempCtrl,
+            link: autoCompleteTempLink,
+            scope: {
+
+            },
+            templateUrl: '../templates/auto-complete.html'
+
+        };
+
+    }
+
+    /**
       * Handles map-temp directive's controller
       *
       * @param {Object} $scope
@@ -33,7 +61,7 @@
                 lat: parseFloat($scope.options.lat),
                 lng: parseFloat($scope.options.lng),
             },
-            zoom: 14
+            zoom: $scope.options.zoom || 18
         };
 
     }
@@ -64,5 +92,46 @@
 
     }
 
+    /**
+      * Handles auto-complete-temp directive's controller
+      *
+      * @param {Object} $scope
+      */
+    function autoCompleteTempCtrl($scope) {
+        $scope.fillInAdress = fillInAdress;
+    }
+
+    /**
+      * Handles auto-complete-temp directive's link
+      *
+      * @param {Object} $scope
+      * @param {Object} element
+      * @param {Object} attrs
+      * @param {Array} ctlrs
+      */
+    function autoCompleteTempLink($scope, element, attrs, ctrls) {
+        
+        console.log(element);
+        
+        // Create autocomplete object
+        $scope.autocomplete = new g.maps.places.Autocomplete(
+            // Dom element
+            element[0].getElementsByTagName('input')[0] || null,
+            // Type
+            { type: 'geocode' }
+        );
+        
+        // Attach an event into autocomplete
+        g.maps.event.addListener($scope.autocomplete, 'place_changed', function() {
+            
+            $scope.fillInAdress();
+            
+        });
+
+    }
+    
+    function fillInAdress() {
+
+    }
 
 }(angular, google));
