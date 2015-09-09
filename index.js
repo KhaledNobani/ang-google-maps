dab6a4dea21af886af8ebedfe926f7de9245d722
KhaledN <khaledn@khaledn.local>
2015-09-09 14:38:15 +0700

    fix(index.js): fixing the variable name

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var @http = require('http'),
-    @server = @http.createServer(function(req, res) {
+var $http = require('http'),
+    $server = $http.createServer(function(req, res) {
         
         res.setRequestHeader('Content-Type', 'text/html');
         res.end('Welcome to ang-google-maps package');
         
     });
 
-@server.listen(8080, 'localhost');
+$server.listen(8080, 'localhost');
 console.log('Listening on port 8080');
